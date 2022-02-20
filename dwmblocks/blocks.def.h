29edb3ab4fcb19de15bc1ab2b0874c35a94f24a3
Golden Joker <gj@localhost.localdomain>
2022-02-20 15:11:04 +0400

    edited dwmblocks

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,11 +1,16 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+// Modify this file to change what commands output to your statusbar, and
+// recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	5,		0},
 
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+    /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
+    {"",      "volume",           1,                1},
+    //{"",      "cpu",		  1,                1},
+    {"",      "ram",              1,                1},
+    {"",      "day",              60,               1},
+    {"",      "clock",            60,               4},
 };
 
-//sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+// sets delimeter between status commands. NULL character ('\0') means no
+// delimeter.
+static char delim[] = " ";
 static unsigned int delimLen = 5;
