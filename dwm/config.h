ba5e4461bd1795bde7a214f50cd8fbf502a64515
Golden Joker <gj@localhost.localdomain>
2022-03-05 11:48:54 +0400

    font

--- dwm/config.h
+++ dwm/config.h
@@ -6,8 +6,8 @@ static const unsigned int gappx     = 7;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono:size=11" };
-static const char dmenufont[]       = "JetBrainsMono:size=11";
+static const char *fonts[]          = { "JetBrainsMono:size=10" };
+static const char dmenufont[]       = "JetBrainsMono:size=10";
 static const char col_gray1[]       = "#1C1F24";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#BBBBBB";
