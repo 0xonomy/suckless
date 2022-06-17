ca95767798599d944252f623e4af85e37da177be
0xonomy <79750374+0xonomy@users.noreply.github.com>
2022-06-17 15:01:50 +0400

    Add files via upload

---
+++ dmenu/themes/solorized.h
@@ -0,0 +1,6 @@
+ static const char *colors[SchemeLast][2] = {
+ 	/*     fg         bg       */
+ 	[SchemeNorm] = { "#839496", "#002b36" },
+	[SchemeSel] = { "#839496", "#073642"},
+ 	[SchemeOut] = { "#000000", "#00ffff" },
+ };
